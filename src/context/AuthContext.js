7c2ca17fb9f415d5d8ad3b523e0bc109b784e6d2
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-05-19 06:54:49 +0600

    project setup with fire base

---
+++ src/context/AuthContext.js
@@ -0,0 +1,59 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth, googleProvider } from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logout = () => {
+    return signOut(auth);
+  };
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+  const value = {
+    user,
+    loading,
+    register,
+    login,
+    googleLogin,
+    logout,
+    updateUserProfile,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
